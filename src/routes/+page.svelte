b4f023605576f9790a4f10ed51b8ac021cb90e94
eric <nukenin.konoha@gmail.com>
2024-04-06 23:26:57 +0800

    feature: fix banner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,78 +5,7 @@
 
 <!-- Banner area start -->
 <section class="bd-banner-area banner-style-two banner-slider-mask">
-	<div class="banner-booking-two">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-xxl-8 col-xl-10 col-lg-11">
-					<div class="banner-search-wrapper style-two wow fadeIn" data-wow-delay="1.2s">
-						<div class="banner-search-box">
-							<div class="banner-search-form">
-								<div class="banner-search-field has-separator d-flex align-items-center gap-10">
-									<div class="search-icon-bg">
-										<span><i class="fa-regular fa-location-dot"></i></span>
-									</div>
-									<div class="banner-search-item banner-select">
-										<p class="b2 mb-0 fw-5">Where to</p>
-										<div class="banner-search-select">
-											<select name="location" id="location">
-												<option>Search location</option>
-												<option>Australia</option>
-												<option>Portugal</option>
-												<option>Maldives</option>
-												<option>India</option>
-											</select>
-										</div>
-									</div>
-								</div>
-								<div class="banner-search-field has-separator d-flex align-items-center gap-10">
-									<div class="search-icon-bg">
-										<span><i class="icon-cleander"></i></span>
-									</div>
-									<div class="banner-search-item">
-										<p class="b2 mb-0 fw-5">Duration</p>
-										<div class="banner-form-input">
-											<input
-												class="form-control"
-												id="selectingMultipleDates"
-												type="text"
-												placeholder="Select Your date"
-												readonly="readonly"
-											/>
-										</div>
-									</div>
-								</div>
-								<div class="banner-search-field d-flex align-items-center gap-10">
-									<div class="search-icon-bg">
-										<span><i class="fa-regular fa-location-dot"></i></span>
-									</div>
-									<div class="banner-search-item banner-select">
-										<p class="b2 mb-0 fw-5">Tour Types</p>
-										<div class="banner-search-select">
-											<select>
-												<option>All Tours</option>
-												<option>Australia</option>
-												<option>Portugal</option>
-												<option>Maldives</option>
-												<option>India</option>
-											</select>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="banner-search-button">
-								<button
-									type="submit"
-									class="banner-btn banner-square-btn bd-btn btn-style radius-10"
-									><span> <i class="fa-regular fa-magnifying-glass"></i></span> Search
-								</button>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+<!--	-->
 	<div class="swiper banner-two-active p-relative">
 		<div class="swiper-wrapper">
 			<div class="swiper-slide">
@@ -90,17 +19,18 @@
 							<div class="col-xxl-9 col-xl-9 col-lg-10">
 								<div class="banner-two-content">
 									<h1 class="banner-title medium fw-7 mb-20 white-text">
-										Mountain Serenity: Finding <span class="yellow-shape"
-											>Peace <img
-												src="assets/images/shapes/yellow-shape.png"
-												alt="yellow-shape"
-											/></span
-										>Amidst Alpine Majesty
+										Embrace the Elements: <span class="yellow-shape"
+									>Tanah Lot's <img
+										src="assets/images/shapes/yellow-shape.png"
+										alt="yellow-shape"
+									/></span
+									> Voyage
+
 									</h1>
 									<p class="banner-description mb-20">
-										A picturesque picnic for two, basking in nature's serenity, forging memories to
-										last a lifetime.
+										Discover the allure of Tanah Lot, where nature and spirituality converge in a mesmerizing embrace. Embark on a picturesque journey for two, crafting timeless memories amidst Bali's coastal wonders.
 									</p>
+
 								</div>
 							</div>
 						</div>
@@ -118,16 +48,18 @@
 							<div class="col-xxl-9 col-xl-9 col-lg-10">
 								<div class="banner-two-content">
 									<h1 class="banner-title medium fw-7 mb-20 white-text">
-										Conquering the Waves: <span class="yellow-shape"
-											>Thrilling <img
-												src="assets/images/shapes/yellow-shape.png"
-												alt="yellow-shape"
-											/></span
-										>Water Mount Adventure
+										Kecak Dance: <span class="yellow-shape"
+										>A Rhythmic <img
+											src="assets/images/shapes/yellow-shape.png"
+											alt="yellow-shape"
+										/></span
+										> Tapestry of Balinese Culture
+
 									</h1>
 									<p class="banner-description mb-20">
-										Riding the waves: A man finds joy and adventure on his water mount.
+										Dive into the enchantment of the Kecak Dance, where ancient myths and rhythmic chants intertwine, offering a captivating glimpse into Balinese culture and tradition.
 									</p>
+
 								</div>
 							</div>
 						</div>
@@ -145,16 +77,15 @@
 							<div class="col-xxl-9 col-xl-9 col-lg-10">
 								<div class="banner-two-content">
 									<h1 class="banner-title medium fw-7 mb-20 white-text">
-										Ulun Danu  <span class="yellow-shape"
-											>Bratan <img
-												src="assets/images/shapes/yellow-shape.png"
-												alt="yellow-shape"
-											/></span
-										>TEst
+										Kelingking Beach: <span class="yellow-shape"
+									>Nature's <img
+										src="assets/images/shapes/yellow-shape.png"
+										alt="yellow-shape"
+									/></span
+									> Majestic Tapestry Unveiled
 									</h1>
 									<p class="banner-description mb-20">
-										Embrace the thrill as couples glide together, savoring the joy of water skating
-										delightfully.
+										Explore the breathtaking beauty of Kelingking Beach, a hidden gem nestled along Bali's rugged coastline. With its iconic cliff formation and pristine sands kissed by azure waters, it's a haven for adventurers and nature enthusiasts seeking unparalleled beauty and serenity
 									</p>
 								</div>
 							</div>
@@ -173,16 +104,16 @@
 							<div class="col-xxl-9 col-xl-9 col-lg-10">
 								<div class="banner-two-content">
 									<h1 class="banner-title medium fw-7 mb-20 white-text">
-										Ulun Danu  <span class="yellow-shape"
-									>Bratan <img
+										Monkey Forest Ubud: A Lush Sanctuary Where Humans Meet Their
+										<span class="yellow-shape"
+									> Mischievous<img
 										src="assets/images/shapes/yellow-shape.png"
 										alt="yellow-shape"
 									/></span
-									>TEst
+									> Cousins
 									</h1>
 									<p class="banner-description mb-20">
-										Embrace the thrill as couples glide together, savoring the joy of water skating
-										delightfully.
+										Step into the enchanting realm of Monkey Forest Ubud, where ancient temples and lush greenery provide a haven for playful macaques. Lose yourself in this mystical sanctuary, where encounters with these spirited creatures offer glimpses into Bali's rich cultural tapestry.
 									</p>
 								</div>
 							</div>
@@ -201,16 +132,16 @@
 							<div class="col-xxl-9 col-xl-9 col-lg-10">
 								<div class="banner-two-content">
 									<h1 class="banner-title medium fw-7 mb-20 white-text">
-										Ulun Danu  <span class="yellow-shape"
-									>Bratan <img
+										Tirta Empul:
+										<span class="yellow-shape"
+									>Sacred Springs <img
 										src="assets/images/shapes/yellow-shape.png"
 										alt="yellow-shape"
 									/></span
-									>TEst
+									>, Spiritual Renewal
 									</h1>
 									<p class="banner-description mb-20">
-										Embrace the thrill as couples glide together, savoring the joy of water skating
-										delightfully.
+										Discover Tirta Empul, an ancient Hindu temple renowned for its sacred springs believed to possess purifying powers. Immerse yourself in the ritual baths where locals and pilgrims seek spiritual cleansing and renewal, amidst the tranquil beauty of Bali's countryside
 									</p>
 								</div>
 							</div>
@@ -229,16 +160,15 @@
 							<div class="col-xxl-9 col-xl-9 col-lg-10">
 								<div class="banner-two-content">
 									<h1 class="banner-title medium fw-7 mb-20 white-text">
-										Ulun Danu  <span class="yellow-shape"
-									>Bratan <img
+										Ulun Danu Bratan: Serenity Upon the Lake's <span class="yellow-shape"
+									>Reflections<img
 										src="assets/images/shapes/yellow-shape.png"
 										alt="yellow-shape"
 									/></span
-									>TEst
+									>
 									</h1>
 									<p class="banner-description mb-20">
-										Embrace the thrill as couples glide together, savoring the joy of water skating
-										delightfully.
+										Ulun Danu Bratan is a serene sanctuary nestled beside Lake Bratan, where the iconic temple complex seems to float upon the water's surface. Delve into the spiritual ambiance as misty mountain vistas and tranquil waters create a picturesque backdrop for contemplation and reverence.
 									</p>
 								</div>
 							</div>
