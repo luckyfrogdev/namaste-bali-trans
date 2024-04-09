dac5b76e3b09eebcf13371068e4b1163e563b5d9
eric <nukenin.konoha@gmail.com>
2024-04-09 10:09:22 +0800

    feature: add command

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #docker stop chat_app && docker rm chat_app &&
-yarn run build &&
+yarn install && run build &&
 docker build -t namaste_bali_trans . &&
 docker run --name namaste_bali_trans -p 3000:3000 -d namaste_bali_trans
