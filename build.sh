7c4241cb07b9edcf423da05a09ca1e6eba610634
eric <nukenin.konoha@gmail.com>
2024-04-10 22:26:13 +0800

    feature: add configuration to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#docker stop chat_app && docker rm chat_app &&
+docker stop namaste_bali_trans && docker rm namaste_bali_trans &&
 yarn install && yarn run build &&
 docker build -t namaste_bali_trans . &&
 docker run --name namaste_bali_trans -p 3000:3000 -d namaste_bali_trans
