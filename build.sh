4d27f8b7e18bf56175dcad1033ff228f793aca2c
eric <nukenin.konoha@gmail.com>
2024-04-11 10:10:26 +0800

    feature: add sudo access

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 if sudo docker ps -a --format '{{.Names}}' | grep -q '^namaste_bali_trans$'; then
     sudo docker stop namaste_bali_trans && sudo docker rm namaste_bali_trans &&
-    npm install && npm run build &&
+    sudo npm install && sudo npm run build &&
     sudo docker build -t namaste_bali_trans . &&
     sudo docker run --name namaste_bali_trans -p 3000:3000 -d namaste_bali_trans
 else
-    npm install && npm run build &&
+   sudo npm install && sudo npm run build &&
        sudo docker build -t namaste_bali_trans . &&
        sudo docker run --name namaste_bali_trans -p 3000:3000 -d namaste_bali_trans
 fi
