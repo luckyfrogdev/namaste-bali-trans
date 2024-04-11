4ae90188e2e6b6a28681eae69997cee54af84a8f
eric <nukenin.konoha@gmail.com>
2024-04-11 10:06:50 +0800

    feature: update github action and build sh file

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-if docker ps -a --format '{{.Names}}' | grep -q '^namaste_bali_trans$'; then
-    docker stop namaste_bali_trans && docker rm namaste_bali_trans &&
+if sudo docker ps -a --format '{{.Names}}' | grep -q '^namaste_bali_trans$'; then
+    sudo docker stop namaste_bali_trans && sudo docker rm namaste_bali_trans &&
     npm install && npm run build &&
-    docker build -t namaste_bali_trans . &&
-    docker run --name namaste_bali_trans -p 3000:3000 -d namaste_bali_trans
+    sudo docker build -t namaste_bali_trans . &&
+    sudo docker run --name namaste_bali_trans -p 3000:3000 -d namaste_bali_trans
 else
     npm install && npm run build &&
-       docker build -t namaste_bali_trans . &&
-       docker run --name namaste_bali_trans -p 3000:3000 -d namaste_bali_trans
+       sudo docker build -t namaste_bali_trans . &&
+       sudo docker run --name namaste_bali_trans -p 3000:3000 -d namaste_bali_trans
 fi
