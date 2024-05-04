be1883308c3f26cf88351d0180ddde9818e6e446
eric <nukenin.konoha@gmail.com>
2024-05-04 15:24:52 +0700

    feature: update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@ if sudo docker ps -a --format '{{.Names}}' | grep -q '^namaste_bali_trans$'; the
     sudo docker stop namaste_bali_trans && sudo docker rm namaste_bali_trans &&
     sudo npm install && sudo npm run build &&
     sudo docker build -t namaste_bali_trans . &&
-    sudo docker run --name namaste_bali_trans -env-file .env -p 3000:3000 -d namaste_bali_trans
+    sudo docker run --name namaste_bali_trans --env-file .env -p 3000:3000 -d namaste_bali_trans
 else
    sudo npm install && sudo npm run build &&
        sudo docker build -t namaste_bali_trans . &&
-       sudo docker run --name namaste_bali_trans -env-file .env -p 3000:3000 -d namaste_bali_trans
+       sudo docker run --name namaste_bali_trans --env-file .env -p 3000:3000 -d namaste_bali_trans
 fi
